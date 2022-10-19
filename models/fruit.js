cb05a0a9c91b6c7a6ce898659b3736feebcf4cdb
bjlovee <bj000051@yahoo.com>
2022-10-19 19:26:15 -0400

    updated code to Standard

--- models/fruit.js
+++ models/fruit.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose')
 
-// Make A Schema 
+// Make A Schema
 const fruitSchema = new mongoose.Schema({
-    name: { type: String, required: true},
-    color: { type: String, required: true},
-    readyToEat: Boolean 
+  name: { type: String, required: true },
+  color: { type: String, required: true },
+  readyToEat: Boolean
 })
-
 
 // Make A model From The Schema
 const Fruit = mongoose.model('Fruit', fruitSchema)
-
 
 // Export The Model For Use In The App
 
