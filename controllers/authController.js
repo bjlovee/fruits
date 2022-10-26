147e2217287dcd503932c0941f9b61d8915a6eb9
bjlovee <bj000051@yahoo.com>
2022-10-25 22:54:39 -0400

    add pseudo code to authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,23 @@
+
+
+// login
+// POST
+//sending datat to the server
+//sending a username and password 
+//Lokkup ther user in the db --- Mongoose Method .findOne({username: req,body.username})
+//compare the req.body.password or credentials to the pass/cred
+//if they match 
+// ---authenticate the user
+
+//signup
+//POST
+//sending  a username and password ...more data
+
+//login View Route
+//GET
+
+//signup  View Route
+//GET
+
+//logout
+//GET
