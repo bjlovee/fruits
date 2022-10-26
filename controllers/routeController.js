44d9c34ee1f3f4fb80f52f5f07bf892ac7c6f74e
bjlovee <bj000051@yahoo.com>
2022-10-25 22:35:44 -0400

    setup seed.js and user.js

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,12 +1,25 @@
-// Start our router
+/// Start our router
 // Import express
 const express = require('express')
 // only the router none of the other app stuff
 const router = express.Router()
 const dataController = require('./dataController')
 const viewController = require('./viewController')
+const apiController = require('./apiController')
 
-// Routes
+// API Routes
+// Index
+router.get('/api', dataController.index, apiController.index)
+// Delete
+router.delete('/api/:id', dataController.destroy, apiController.show)
+// Update
+router.put('/api/:id', dataController.update, apiController.show)
+// Create
+router.post('/api', dataController.create, apiController.show)
+// Show
+router.get('/api/:id', dataController.show, apiController.show)
+
+// Non API Routes
 // Index
 router.get('/', dataController.index, viewController.index)
 // New
